98968dba68c070ec4e2b688eb045648019c66dbc
heonq <gkagjsrb@naver.com>
2023-06-20 19:57:42 +0900

    feat : 입력 메시지를 출력하고 사용자에게 입력을 받는 기능

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,29 @@
 const {print,generateRandomNumber} = require("./util.js");
 const {GAME_MESSAGE} = require("./constants.js");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class Game {
     
     play(){
         this.start();
         this.setUp();
+        this.getUserInput();
     }
+
     start(){
         this.printStartMessage();
     }
+
     setUp(){
         this.correctNumber = generateRandomNumber();
+    
+    }
+
+    getUserInput(){
+        MissionUtils.Console.readLine(GAME_MESSAGE.INPUT,(answer)=>{
+            this.user = answer;
+            this.getUserInput();
+        })
     }
     
 
