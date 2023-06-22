69aef0bc5bb8995a17bc64af3f686a8ec74dc753
heonq <gkagjsrb@naver.com>
2023-06-22 15:14:57 +0900

    test : 입력한 값의 각 자리에 중복되는 값이 있을 경우 에러가 발생하는 테스트 코드 작성

--- src/checkUserInput.test.js
+++ src/checkUserInput.test.js
@@ -6,4 +6,8 @@ describe("입력한 값에 대한 유효성 검사",()=>{
         const answers = ["1","13","1425"];
         expect(()=>answers.forEach((answer)=>checkUserInput(answer))).toThrow(Error(ERROR_MESSAGE.THREE_DIGITS));
     })
+    test("입력한 값이 각 자리마다 중복되지 않은 값인지 테스트",()=>{
+        const answers = ["112","133","121"];
+        expect(()=>answers.forEach((answer)=>checkUserInput(answer))).toThrow(Error(ERROR_MESSAGE.DUPLICATED));
+    })
   })
