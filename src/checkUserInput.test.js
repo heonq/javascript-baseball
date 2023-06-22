cb37f95c3957939a7a81baa0cf2f38dc273842e7
heonq <gkagjsrb@naver.com>
2023-06-22 15:16:44 +0900

    test : 입력된 값에 1-9까지의 숫자가 아닌 문자가 있을 경우 에러가 발생하는 테스트 코드 작성

--- src/checkUserInput.test.js
+++ src/checkUserInput.test.js
@@ -10,4 +10,8 @@ describe("입력한 값에 대한 유효성 검사",()=>{
         const answers = ["112","133","121"];
         expect(()=>answers.forEach((answer)=>checkUserInput(answer))).toThrow(Error(ERROR_MESSAGE.DUPLICATED));
     })
+    test("문자가 포함하지 않은 1-9까지의 숫자를 입력했는지 테스트",()=>{
+        const answers = ["abc","!@#","ㄱㄴㄷ"];
+        expect(()=>answers.forEach((answer)=>checkUserInput(answer))).toThrow(Error(ERROR_MESSAGE.NOTANUMBER));
+    })
   })
