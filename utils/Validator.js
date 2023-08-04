7c4be1afee6c9762bdeb40bf81f53ac74c2cd6d4
heonq <gkagjsrb@naver.com>
2023-08-04 17:08:50 +0900

    feat: 재시작 숫자에 대한 유효성 검사 기능 구현

--- utils/Validator.js
+++ utils/Validator.js
@@ -3,9 +3,13 @@ const ERROR_MESSAGE = require('./ErrorMessage');
 const Validator = {
   validateNumber(number) {
     if (number.length !== 3) throw new Error(ERROR_MESSAGE.threeDigits);
-    if (number.filter((n) => n >= 1 && n <= 9).length !== 3)
+    if ([...number].filter((n) => n >= 1 && n <= 9).length !== 3)
       throw new Error(ERROR_MESSAGE.digitInRange);
-    if (new Set(number).size !== 3) throw new Error(ERROR_MESSAGE.duplicated);
+    if (new Set([...number]).size !== 3) throw new Error(ERROR_MESSAGE.duplicated);
+  },
+
+  validateCommand(command) {
+    if (+command !== 1 && +command !== 2) throw new Error(ERROR_message.oneOrTwo);
   },
 };
 
