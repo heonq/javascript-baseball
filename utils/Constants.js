d5a841caafb098dad99dd0bd914db5ac6529e449
heonq <gkagjsrb@naver.com>
2023-08-04 15:04:44 +0900

    feat: 게임 진행을 위한 상수 파일 생성

---
+++ utils/Constants.js
@@ -0,0 +1,7 @@
+const CONSTANTS = object.freeze({
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱',
+  restart: 1,
+  quit: 2,
+});
---
+++ utils/Message.js
@@ -0,0 +1,6 @@
+const MESSAGE = Object.freeze({
+  start: '숫자 야구 게임을 시작합니다.\n',
+  askInput: '숫자를 입력해주세요 : ',
+  win: '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n',
+  askRestart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+});
