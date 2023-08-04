29ec370ecab5be320de20b4f918b70487df5bd1f
heonq <gkagjsrb@naver.com>
2023-08-04 14:40:40 +0900

    chore: eslint,prettier 설정 - Airbnb 스타일 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    node: true,
+    commonjs: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: latest,
+  },
+  extends: ["airbnb"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+
+    "max-depth": ["error", 2],
+    "max-lines-per-function": ["error", 10],
+    "max-params": ["error", 3],
+  },
+  overrides: [
+    {
+      files: ["__tests__/**/*.js"],
+      rules: {
+        "max-lines-per-function": "off",
+        "no-new": "off",
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  ],
+};
