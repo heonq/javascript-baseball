47194d37bcee4c0562962dd17262e6047789b59b
heonq <gkagjsrb@naver.com>
2023-08-05 16:57:45 +0900

    style: 에러 메시지에 띄어쓰기 수정

--- utils/ErrorMessage.js
+++ utils/ErrorMessage.js
@@ -2,7 +2,7 @@ const PREFIX = '[ERROR]';
 
 const ERROR_MESSAGE = Object.freeze({
   threeDigits: `${PREFIX}세자리의 숫자를 입력해주세요.`,
-  digitInRange: `${PREFIX}각 자리 수가 1에서 9까지인 숫자를입력해 주세요.`,
+  digitInRange: `${PREFIX}각 자리 수가 1에서 9까지인 숫자를 입력해 주세요.`,
   duplicated: `${PREFIX}서로 중복되지 않은 세자리의 숫자를 입력해주세요.`,
   oneOrTwo: `${PREFIX}1과 2중 하나의 값을 입력해주세요.`,
 });
