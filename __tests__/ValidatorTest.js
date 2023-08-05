6708fee0c497f46a7b4a316fdbb993166c24c2e9
heonq <gkagjsrb@naver.com>
2023-08-05 17:41:48 +0900

    test: 사용자가 입력한 값에 대한 유효성 검사 테스트 구현

---
+++ __tests__/ValidatorTest.js
@@ -0,0 +1,14 @@
+const Validator = require('../utils/Validator');
+const ERROR_MESSAGE = require('../utils/ErrorMessage');
+
+describe('유효성 검사 테스트', () => {
+  test.each([
+    [['1', '12', '1234'], ERROR_MESSAGE.threeDigits],
+    [['019', '102', '320'], ERROR_MESSAGE.digitInRange],
+    [['112', '121', '211'], ERROR_MESSAGE.duplicated],
+  ])('사용자가 입력한 값에 대한 유효성 검사', (userInput, errorMessage) => {
+    userInput.forEach((input) => {
+      expect(() => Validator.validateNumber(input)).toThrow(errorMessage);
+    });
+  });
+});
